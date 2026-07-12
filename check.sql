6207d72271cfc0ebd665f58cd98d49d952049347
Vinícius Bispo <Admin@DESKTOP-09G4N5S>
2026-07-12 09:29:43 -0300

    removendo consulta sql do repositório

--- check.sql
+++
@@ -1,8 +0,0 @@
-USE DB_LOCADORA;
-DELETE FROM Emprestimos;
-DELETE FROM JogoDatasLancamento;
-DELETE FROM JogoDesenvolvedor;
-DELETE FROM JogoGenero;
-DELETE FROM JogoPublicadora;
-DELETE FROM Jogos;
-SELECT COUNT(*) AS TotalJogosAposLimpeza FROM Jogos;
